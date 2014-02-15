99f7bde5024f5391c01dba89b29b1e3c56e838cd
Lefteris <teris.tat@gmail.com>
2014-02-15 14:21:59 +0000

    js tests

--- voidEditor/src/main/webapp/js/controllers.js
+++ voidEditor/src/main/webapp/js/controllers.js
@@ -41,9 +41,8 @@ editorAppControllers.controller('editorCtrl', [  '$scope', '$rootScope', 'voidDa
         $rootScope.haveStatsFinished = 1;
 
         var ua = window.navigator.userAgent
-        $rootScope.msie = ua.indexOf ( ".NET " );
+        $rootScope.msie = ua.indexOf ( ".NET" );
 
-        console.log(ua);
         $rootScope.otherLicence = function (val) {
             if (val == "other") {
                 $rootScope.data.licence = "";
