ceee63f1ba5f80aa4fc732ae0f559cae3df9240b
Lefteris <teris.tat@gmail.com>
2014-06-17 14:00:27 +0100

    Removed files

--- voidEditor/src/main/webapp/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* App Module */
-
-var editorApp = angular.module('editorApp', ['ngRoute' , 'editorAppControllers', 'editorAppDirectives', 'ngSanitize'
-    , 'ui.bootstrap' , 'modalControllers' , 'dateControllers']);
-
-
